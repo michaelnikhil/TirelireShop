9fdf0ddfea3303222950d2434ca5e683b88c6f72
michael.n.descamps <michael.n.descamps@gmail.com>
2020-08-13 14:10:33 +0000

    Added details view for controllers controller Produit, Fabricant, Couleur

--- TirelireShop/Controllers/FabricantController.cs
+++ TirelireShop/Controllers/FabricantController.cs
@@ -4,15 +4,30 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TirelireShop.DataAccess;
+using TirelireShop.Repository;
 
 namespace TirelireShop.Controllers
 {
     public class FabricantController : Controller
     {
+
+        private IRepository<Fabricant> repoFabricant;
+        private DBTirelireShopContext ctx;
+
+
+        public FabricantController()
+        {
+            ctx = new DBTirelireShopContext();
+            repoFabricant = new RepositoryTirelire<Fabricant>(ctx);
+        }
+
+
         // GET: FabricantController
         public ActionResult Index()
         {
-            return View();
+            return View(repoFabricant.GetAll());
+
         }
 
         // GET: FabricantController/Details/5
