c94f21a706a49f72099c0f5bd02485bf5ed9a14f
michael.n.descamps <michael.n.descamps@gmail.com>
2020-08-13 15:12:04 +0000

    Home page with CSS preprare for the gallery view

--- TirelireShop/Controllers/HomeController.cs
+++ TirelireShop/Controllers/HomeController.cs
@@ -3,24 +3,31 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using TirelireShop.DataAccess;
 using TirelireShop.Models;
+using TirelireShop.Repository;
 
 namespace TirelireShop.Controllers
 {
     public class HomeController : Controller
     {
+        private IRepository<Produit> repoProduit;
+        private DBTirelireShopContext ctx;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(  ILogger<HomeController> logger)
         {
+            ctx = new DBTirelireShopContext();
+            repoProduit = new RepositoryTirelire<Produit>(ctx);
             _logger = logger;
         }
 
         public IActionResult Index()
         {
-            return View();
+            return View(repoProduit.GetAll());
         }
 
         public IActionResult Privacy()
